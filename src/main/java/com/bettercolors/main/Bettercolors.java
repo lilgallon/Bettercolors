21cafc60acc06afff9211d4c55edd24f9510c712
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2018-08-13 20:44:30 +0200

    fixed key input handling

--- src/main/java/com/bettercolors/main/Bettercolors.java
+++ src/main/java/com/bettercolors/main/Bettercolors.java
@@ -12,11 +12,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 import org.lwjgl.input.Keyboard;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 @Mod(modid = Reference.MOD_ID,
@@ -29,16 +31,18 @@ public class Bettercolors {
     private final static ArrayList<Option> DEFAULT_ACTIVATION_STATUS;
     static{
         DEFAULT_ACTIVATION_STATUS = new ArrayList<>();
-        DEFAULT_ACTIVATION_STATUS.add(new ToggleOption(AimAssistance.class.getName(), true));
-        DEFAULT_ACTIVATION_STATUS.add(new ToggleOption(ClickAssistance.class.getName(), true));
+        DEFAULT_ACTIVATION_STATUS.add(new ToggleOption(AimAssistance.class.getSimpleName(), true));
+        DEFAULT_ACTIVATION_STATUS.add(new ToggleOption(ClickAssistance.class.getSimpleName(), true));
     }
 
-    private ArrayList<Option> activation_status;
+    private ArrayList<Option> _activation_status;
 
     private static int KEY_PAGE_UP = 201;
     private ArrayList<Module> _mods;
+    
+    private Map<String, Boolean> _key_down;
 
-	@EventHandler
+    @EventHandler
 	public void Init(FMLInitializationEvent event)
 	{
 		MinecraftForge.EVENT_BUS.register(this);
@@ -49,8 +53,8 @@ public class Bettercolors {
         ArrayList<ArrayList<Option>> modules_options = new ArrayList<>();
         modules_options.add(AimAssistance.getDefaultOptions());
         modules_options.add(ClickAssistance.getDefaultOptions());
-        activation_status = DEFAULT_ACTIVATION_STATUS;
-        modules_options.add(activation_status);
+        _activation_status = DEFAULT_ACTIVATION_STATUS;
+        modules_options.add(_activation_status);
         // There is no settings file, we need to create it, otherwise we only add the settings that are not already
         // in the settings file (can happen after updating the mod to a newer version)
         SettingsUtils.setOptions(modules_options, options != null);
@@ -58,8 +62,14 @@ public class Bettercolors {
 
 		// Mods initialisation
 		_mods = new ArrayList<>();
-		_mods.add(new AimAssistance("Aim assistance", Keyboard.KEY_HOME, Boolean.parseBoolean(options.get(AimAssistance.class.getName())), options));
-		_mods.add(new ClickAssistance("Click assistance", KEY_PAGE_UP, Boolean.parseBoolean(options.get(ClickAssistance.class.getName())), options));
+		_mods.add(new AimAssistance("Aim assistance", Keyboard.KEY_HOME, Boolean.parseBoolean(options.get(AimAssistance.class.getSimpleName())), options));
+		_mods.add(new ClickAssistance("Click assistance", KEY_PAGE_UP, Boolean.parseBoolean(options.get(ClickAssistance.class.getSimpleName())), options));
+
+		// KeyEvent
+        _key_down = new HashMap<>();
+        _key_down.put(AimAssistance.class.getSimpleName(), false);
+        _key_down.put(ClickAssistance.class.getSimpleName(), false);
+
 
 		// Window initialisation
         // todo
@@ -71,15 +81,24 @@ public class Bettercolors {
 	@SubscribeEvent
 	public void onClientTickEvent(ClientTickEvent event){
         // todo : Window updating acc. to mods
+        for(Module mod : _mods){
+            if(Keyboard.isKeyDown(mod.getToggleKey())){
+                _key_down.replace(mod.getClass().getSimpleName(), true);
+            }else if(_key_down.get(mod.getClass().getSimpleName())){
+                // KEY RELEASED !
+                mod.toggle();
+                SettingsUtils.setOption(mod.getClass().getSimpleName(), Boolean.toString(mod.isActivated()));
+                _key_down.replace(mod.getClass().getSimpleName(), false);
+            }
+        }
 	}
 
 	@SubscribeEvent
 	public void clientTick(final TickEvent event){
         for(Module mod : _mods){
-            if(Keyboard.isKeyDown(mod.getToggleKey())){
-                mod.toggle();
+            if(mod.isActivated()){
+                mod.onUpdate();
             }
-            mod.onUpdate();
         }
 	}
 }

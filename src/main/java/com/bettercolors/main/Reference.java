4557e178277906a9ba95977a0664f36f82c47c76
Lilian Gallon <lilian.gallon@etu.u-bordeaux.fr>
2018-11-10 15:12:37 +0100

    update version number

--- src/main/java/com/bettercolors/main/Reference.java
+++ src/main/java/com/bettercolors/main/Reference.java
@@ -3,6 +3,6 @@ package com.bettercolors.main;
 public class Reference {
 	public static final String MOD_ID = "Bettercolors";
 	public static final String NAME = "N3RO's Bettercolors";
-	public static final String VERSION = "6.0-b2";
+	public static final String VERSION = "6.0-b3";
 	public static final String ACCEPTED_VERSIONS = "[1.8.9]";
 }

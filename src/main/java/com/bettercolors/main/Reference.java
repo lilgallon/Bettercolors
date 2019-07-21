7ef929695af81765cdc66dc6d392071eec8d3c21
nero <swellchaser33@gmail.com>
2019-07-21 13:51:38 +0200

    update forge from 1.13.2 to 1.14.4

--- src/main/java/com/bettercolors/main/Reference.java
+++ src/main/java/com/bettercolors/main/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
 	public static final String MOD_ID = "bettercolors";
 	public static final String NAME = "N3RO's Bettercolors";
 	public static final String VERSION = "6.0.0";
-	public static final String ACCEPTED_VERSIONS = "[1.13.2]";
-	public static final String MAIN_MC_VERSION = "1.13.2";
+	public static final String ACCEPTED_VERSIONS = "[1.14.4]";
+	public static final String MAIN_MC_VERSION = "1.14.4";
 }

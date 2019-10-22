e6ed42455d5e31e3722f19b28550f4311c762a24
Lilian Gallon <swellchaser33@gmail.com>
2019-10-21 22:24:32 -0700

    #28: changed modid to make sure that bettercolors is not recognizable

--- src/main/java/com/bettercolors/main/Reference.java
+++ src/main/java/com/bettercolors/main/Reference.java
@@ -1,7 +1,7 @@
 package com.bettercolors.main;
 
 public class Reference {
-	public static final String MOD_ID = "Bettercolors";
+	public static final String MOD_ID = "minecraft";
 	public static final String NAME = "N3RO's Bettercolors";
 	public static final String VERSION = "6.0.0";
 	public static final String MAIN_MC_VERSION = "1.8.9"; // used by bettercolors version checker

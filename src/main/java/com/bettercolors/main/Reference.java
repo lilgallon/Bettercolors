0b899de983f221ddbc9a8f09caed020d341ce695
Lilian Gallon <swellchaser33@gmail.com>
2019-10-21 18:26:00 -0700

    #28: changed modid to make sure that bettercolors is not recognizable

--- src/main/java/com/bettercolors/main/Reference.java
+++ src/main/java/com/bettercolors/main/Reference.java
@@ -1,7 +1,7 @@
 package com.bettercolors.main;
 
 public class Reference {
-	public static final String MOD_ID = "bettercolors";
+	public static final String MOD_ID = "mcp";
 	public static final String NAME = "N3RO's Bettercolors";
 	public static final String VERSION = "6.0.0";
 	public static final String ACCEPTED_VERSIONS = "[1.14.4]";

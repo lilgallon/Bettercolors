b518be9fbade833a1878dc01c2c39b109abe5e6b
N3ROO <swellchaser33@gmail.com>
2019-12-14 05:58:33 -0800

    added acknowledgements in console & readme

--- src/main/java/dev/nero/bettercolors/main/Reference.java
+++ src/main/java/dev/nero/bettercolors/main/Reference.java
@@ -3,7 +3,7 @@ package dev.nero.bettercolors.main;
 public class Reference {
 	public static final String MOD_ID = "mcp";
 	public static final String NAME = "N3RO's Bettercolors";
-	public static final String VERSION = "6.0.1";
+	public static final String VERSION = "6.1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.14.4]";
 	public static final String MAIN_MC_VERSION = "1.14.4";
 }

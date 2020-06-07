ea4a01b1be81f9eddc4afa5b3f6e9bcab4d0f1d3
N3ROO <swellchaser33@gmail.com>
2020-06-07 17:34:02 +0200

    reformatted TimeHelper.java

--- src/main/java/dev/nero/bettercolors/utils/TimeHelper.java
+++ src/main/java/dev/nero/bettercolors/utils/TimeHelper.java
@@ -1,9 +1,28 @@
+/*
+ * Copyright 2018-2020 Bettercolors Contributors (https://github.com/N3ROO/Bettercolors)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.nero.bettercolors.utils;
 
 public class TimeHelper {
 	private long lastMS = 0L;
 	private boolean stopped;
 
+	/**
+	 * The timer is stopped by default.
+	 */
 	public TimeHelper(){
 		stopped = true;
 	}
@@ -24,10 +43,16 @@ public class TimeHelper {
 		reset();
 	}
 
+	/**
+	 * It resets the timer to 0
+	 */
     public void reset(){
 	    this.lastMS = getCurrentMS();
     }
 
+	/**
+	 * @return true whether the timer is stopped or not
+	 */
 	public boolean isStopped(){
 		return stopped;
 	}
@@ -43,6 +68,9 @@ public class TimeHelper {
 		}else return delay <= 0;
 	}
 
+	/**
+	 * @return current system milliseconds
+	 */
 	private long getCurrentMS(){
 		return System.nanoTime() / 1000000L;
 	}

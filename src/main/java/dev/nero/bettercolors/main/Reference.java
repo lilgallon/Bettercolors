2d3df6ea746258af645f9bf1f70c313391a6d6bb
N3ROO <swellchaser33@gmail.com>
2020-06-29 19:36:29 +0200

    updated workflow & stuff about 1.15.2 to 1.16.1

--- src/main/java/dev/nero/bettercolors/main/Reference.java
+++ src/main/java/dev/nero/bettercolors/main/Reference.java
@@ -3,9 +3,9 @@ package dev.nero.bettercolors.main;
 public class Reference {
 	public static final String MOD_ID = "mcp";
 	public static final String NAME = "N3RO's Bettercolors";
-	public static final String VERSION = "6.1.1";
-	public static final String ACCEPTED_VERSIONS = "[1.15.2]";
-	public static final String MAIN_MC_VERSION = "1.15.2";
+	public static final String VERSION = "6.1.2";
+	public static final String ACCEPTED_VERSIONS = "[1.16.1]";
+	public static final String MAIN_MC_VERSION = "1.16.1";
 	public final static String ISSUE_TRACKER = "https://github.com/N3ROO/Bettercolors/issues";
 	public final static String DOWNLOAD_URL = "https://github.com/N3ROO/Bettercolors/releases/latest";
 }

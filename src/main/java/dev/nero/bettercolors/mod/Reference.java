439190cd92a41f5a8f1c405ea871f961e47df80c
N3ROO <swellchaser33@gmail.com>
2020-07-30 23:16:59 +0200

    bump 6.2.0 -> 7.0.0

--- src/main/java/dev/nero/bettercolors/mod/Reference.java
+++ src/main/java/dev/nero/bettercolors/mod/Reference.java
@@ -4,6 +4,6 @@ public class Reference {
     public static final String MOD_ID = "mcp";
     public static final String MOD_NAME = "bettercolors";
     public static final String MC_VERSION = "1.16.1";
-    public static final String MOD_VERSION = "6.2.0";
+    public static final String MOD_VERSION = "7.0.0";
     public static final String ACCEPTED_MC_VERSIONS = "[1.16.1]";
 }

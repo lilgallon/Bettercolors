c896f9eac179e5839a3eb28461d8df8f8e4011e4
N3ROO <swellchaser33@gmail.com>
2020-08-23 19:15:14 +0200

    added settings to the antibot

--- src/main/java/dev/nero/bettercolors/core/modules/Antibot.java
+++ src/main/java/dev/nero/bettercolors/core/modules/Antibot.java
@@ -4,17 +4,18 @@ import dev.nero.bettercolors.core.events.EventType;
 import dev.nero.bettercolors.core.wrapper.Wrapper;
 import dev.nero.bettercolors.engine.module.Module;
 import dev.nero.bettercolors.engine.option.Option;
+import dev.nero.bettercolors.engine.option.ToggleOption;
+import dev.nero.bettercolors.engine.option.ValueOption;
 import net.minecraft.client.network.play.NetworkPlayerInfo;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.EntityLeaveWorldEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.world.WorldEvent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Antibot extends Module {
 
@@ -23,12 +24,34 @@ public class Antibot extends Module {
     // Prefix for Antibot (logging and settings)
     private static final String PREFIX = "ANTIBOT";
 
+    // Options name
+    private static final String TAB_CHECK = "Tab_check";
+    private static final String PING_CHECK = "Ping_check";
+    private static final String TICKS_LIVED_CHECK = "Ticks_lived_check";
+    private static final String HAS_BEEN_HIT_CHECK = "Has_been_hit_check";
+    private static final String HAS_MADE_DAMAGE_CHECK = "Has_made_damage_check";
+    private static final String TICKS_LIVED_MIN = "Ticks_lived_min";
+
+    // Options index
+    private static final int I_TAB_CHECK = 0;
+    private static final int I_PING_CHECK = 1;
+    private static final int I_TICKS_LIVED_CHECK = 2;
+    private static final int I_HAS_BEEN_HIT_CHECK = 3;
+    private static final int I_HAS_MADE_DAMAGE_CHECK = 4;
+    private static final int I_TICKS_LIVED_MIN = 5;
+
     private static final ArrayList<Option> DEFAULT_OPTIONS;
     static {
         DEFAULT_OPTIONS = new ArrayList<>();
-    }
 
-    HashMap<Integer, Data> entities;
+        DEFAULT_OPTIONS.add(new ToggleOption(PREFIX, TAB_CHECK, true));
+        DEFAULT_OPTIONS.add(new ToggleOption(PREFIX, PING_CHECK, true));
+        DEFAULT_OPTIONS.add(new ToggleOption(PREFIX, TICKS_LIVED_CHECK, true));
+        DEFAULT_OPTIONS.add(new ToggleOption(PREFIX, HAS_BEEN_HIT_CHECK, true));
+        DEFAULT_OPTIONS.add(new ToggleOption(PREFIX, HAS_MADE_DAMAGE_CHECK, true));
+
+        DEFAULT_OPTIONS.add(new ValueOption(PREFIX, TICKS_LIVED_MIN, 40, 1, 100, 5, 20));
+    }
 
     class Data {
         public int ticksLived = 0;
@@ -36,15 +59,19 @@ public class Antibot extends Module {
         public boolean attackedSomeone = false;
     }
 
-    private final int LEGIT_ALIVE_TICKS = 40;
+    HashMap<Integer, Data> entities;
+    private boolean entitiesLoaded = false;
 
     /**
      * @param toggleKey   the toggle Key (-1 -> none).
      * @param isActivated the initial state.
      */
-    public Antibot(Integer toggleKey, Boolean isActivated) {
+    public Antibot(Integer toggleKey, Boolean isActivated, Map<String, String> givenOptions) {
         super("Antibot", toggleKey, isActivated, "shield.png", PREFIX);
+        this.loadOptionsAccordingTo(DEFAULT_OPTIONS, givenOptions);
+
         this.entities = new HashMap<>();
+        this.entitiesLoaded = false;
         instance = this;
     }
 
@@ -54,13 +81,8 @@ public class Antibot extends Module {
         switch (code) {
             case EventType.WORLD_LOAD:
                 this.entities = new HashMap<>();
+                this.entitiesLoaded = false;
 
-                /*
-                WorldEvent.Load worldEvent = (WorldEvent.Load) details;
-                worldEvent.getWorld().getPlayers().forEach((entity -> this.entities.put(
-                        entity.getEntityId(),
-                        new Data()
-                )));*/
                 break;
 
             case EventType.ENTITY_JOIN:
@@ -82,9 +104,18 @@ public class Antibot extends Module {
                 break;
 
             case EventType.WORLD_TICK:
+                if (!this.entitiesLoaded && Wrapper.MC.world != null) {
+                    Wrapper.MC.world.getPlayers().forEach((entity -> this.entities.put(
+                            entity.getEntityId(),
+                            new Data()
+                    )));
+
+                    this.entitiesLoaded = true;
+                }
+
                 for (Data data : this.entities.values()) {
                     // Don't update if it's already above the legit ticks to prevent integer overflow
-                    if (data.ticksLived > LEGIT_ALIVE_TICKS) continue;
+                    if (data.ticksLived > this.getOptionI(I_TICKS_LIVED_MIN)) continue;
                     data.ticksLived ++;
                 }
                 break;
@@ -125,17 +156,30 @@ public class Antibot extends Module {
         if (!(entity instanceof PlayerEntity)) return true;
         PlayerEntity player = (PlayerEntity) entity;
 
+        // Settings
+        final boolean TAB_CHECK = this.getOptionB(I_TAB_CHECK);
+        final boolean PING_CHECK = this.getOptionB(I_PING_CHECK);
+        final boolean TICKS_LIVED_CHECK = this.getOptionB(I_TICKS_LIVED_CHECK);
+        final boolean HAS_BEEN_HIT_CHECK = this.getOptionB(I_HAS_BEEN_HIT_CHECK);
+        final boolean HAS_MADE_DAMAGE_CHECK = this.getOptionB(I_HAS_MADE_DAMAGE_CHECK);
+        final int TICKS_LIVED_MIN = this.getOptionI(I_TICKS_LIVED_MIN);
+
         // If it's not in the tab, it's a bot
-        if (!isInTab(player)) return true;
+        if (!isInTab(player) && TAB_CHECK) return true;
+
         // If it has 0 ms, it's a bot
-        if (Wrapper.MC.getConnection().getPlayerInfo(player.getUniqueID()).getResponseTime() == 0) return true;
+        if (Wrapper.MC.getConnection().getPlayerInfo(player.getUniqueID()).getResponseTime() == 0 && PING_CHECK) return true;
 
         Data data = this.entities.get(player.getEntityId());
         if (data != null) {
             // If it has lived less than 40 ticks, it's a bot
-            if (data.ticksLived < LEGIT_ALIVE_TICKS) return true;
-            // If it has not been hit yet, it's a bot
-            if (!data.hasBeenHit) return true;
+            if (data.ticksLived < TICKS_LIVED_MIN && TICKS_LIVED_CHECK) return true;
+
+            // If it has not been hit yet (received damage), it's a bot
+            if (!data.hasBeenHit && HAS_BEEN_HIT_CHECK) return true;
+
+            // If it has attacked something (and made damage), it's a bot
+            if (!data.attackedSomeone && HAS_MADE_DAMAGE_CHECK) return true;
         }
 
         return false;

b77ac57ab214db0d96429f66805e291c32ae77e5
N3ROO <swellchaser33@gmail.com>
2020-08-24 19:51:38 +0200

    updated some stuff about forge

--- src/main/java/dev/nero/bettercolors/Reference.java
+++ src/main/java/dev/nero/bettercolors/Reference.java
@@ -2,7 +2,7 @@ package dev.nero.bettercolors;
 
 public class Reference {
     public static final String MOD_ID = "mcp";
-    public static final String MOD_VERSION_SUFFIX = "dev";
+    public static final String MOD_VERSION_SUFFIX = "";
     public static final String MC_VERSION = "1.16.2";
     public static final String MOD_VERSION = "7.3.0";
 }

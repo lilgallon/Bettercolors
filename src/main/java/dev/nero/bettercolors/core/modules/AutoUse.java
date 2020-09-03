fd65f4c6f3c4e1cee54e89fdb40f17f0f57ac789
N3ROO <swellchaser33@gmail.com>
2020-09-03 14:31:42 +0200

    #73: fixed strange behaviour with multiple swords

--- src/main/java/dev/nero/bettercolors/core/modules/AutoUse.java
+++ src/main/java/dev/nero/bettercolors/core/modules/AutoUse.java
@@ -39,21 +39,39 @@ public class AutoUse extends Module {
                 if (currSlot != this.prevSlot && this.prevSlot != -1) {
                     // Now we now that the player changed its slot
 
-                    // Check if the player was using a sword (or if we already know where the sword is in the hotbar)
-                    if (Wrapper.MC.player.inventory.mainInventory.get(this.swordSlot).getItem() instanceof SwordItem) {
-                        // We already know where the sword is
-                        Wrapper.rightClick();
-                        usingTimer.start();
-                    } else if (Wrapper.MC.player.inventory.mainInventory.get(this.prevSlot).getItem() instanceof SwordItem) {
-                        // We found the sword
-                        Wrapper.rightClick();
-                        usingTimer.start();
+                    // True means that we will use (right click) and switch to the last slot
+                    boolean use = false;
+
+                    // Check if we already know where the sword is
+                    if (isSword(this.swordSlot)) {
+                        // We will use that slot
+                        use = true;
+                    }
+                    // If not, then check the previous slot
+                    else if (isSword(this.prevSlot)) {
+                        // If we found a slot, we save it
                         this.swordSlot = this.prevSlot;
+                        // Then we will use that slot
+                        use = true;
+                    }
+
+                    if (use) {
+                        Wrapper.rightClick();
+                        usingTimer.start();
                     }
                 }
             }
 
             this.prevSlot = currSlot;
         }
+    }
+
+    /**
+     * @param slot the slot (returns false if invalid)
+     * @return true if the item at the given slot is a sword
+     */
+    private boolean isSword(int slot) {
+        if (slot < 0 || slot > 9) return false;
+        return Wrapper.MC.player.inventory.mainInventory.get(slot).getItem() instanceof SwordItem;
     }
 }

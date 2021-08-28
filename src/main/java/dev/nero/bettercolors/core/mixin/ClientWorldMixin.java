8616431af8d5f15cc4ff94457a694fbcc3f9ceac
Gallon Lilian <lilian@gallon.dev>
2021-08-28 13:56:39 +0200

    Finished porting 1.16 fabric version to 1.17

--- src/main/java/dev/nero/bettercolors/core/mixin/ClientWorldMixin.java
+++ src/main/java/dev/nero/bettercolors/core/mixin/ClientWorldMixin.java
@@ -3,6 +3,7 @@ package dev.nero.bettercolors.core.mixin;
 import dev.nero.bettercolors.core.events.OnEntityJoinCallback;
 import dev.nero.bettercolors.core.events.OnEntityLeaveCallback;
 import dev.nero.bettercolors.core.events.OnWorldLoadCallback;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,8 +24,14 @@ public class ClientWorldMixin {
         OnEntityJoinCallback.EVENT.invoker().trigger(entity);
     }
 
-    @Inject(method = "removeEntity(ILnet/minecraft/entity/Entity.RemovalReason;)V", at = @At("TAIL"))
-    private void entityLeaveEvent(int code, Entity.RemovalReason reason, CallbackInfo info) {
-        OnEntityLeaveCallback.EVENT.invoker().trigger(entity);
+    @Inject(method = "removeEntity(ILnet/minecraft/entity/Entity$RemovalReason;)V", at = @At("TAIL"))
+    private void entityLeaveEvent(int entityId, Entity.RemovalReason reason, CallbackInfo info) {
+        Entity entity;
+        if (MinecraftClient.getInstance().world != null) {
+            entity = MinecraftClient.getInstance().world.getEntityById(entityId);
+            if (entity != null) {
+                OnEntityLeaveCallback.EVENT.invoker().trigger(entity);
+            }
+        }
     }
 }
